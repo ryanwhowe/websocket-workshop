6eb110f5381449804dd285a0606b2f7fe0e84df5
m1ke <git@m1ke.me>
2019-03-25 21:24:40 +0000

    Basic php app with simple database concept

--- lesson-4/php/index.php
+++ lesson-4/php/index.php
@@ -1,15 +1,54 @@
 <?php
 
-use Psr\Http\Message\RequestInterface;
+use SleekDB\SleekDB;
 use Slim\App;
+use Slim\Http\Request;
 use Slim\Http\Response;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = new App();
+$config = [
+	'settings' => [
+		'displayErrorDetails' => true,
+		'logger' => [
+			'name' => 'slim-app',
+			'level' => 'debug',
+			'path' => __DIR__ . '/logs/app.log',
+		],
+	],
+];
+$app = new App($config);
 
-$app->get('/hello/{name}', function (RequestInterface $request, Response $response, $args) {
+$container = $app->getContainer();
+$container['sleek'] = function($container){
+	$users = SleekDB::store('users', __DIR__.'/db');
+
+	return (object) ['users' => $users];
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
 	return $response->getBody()->write("Hello, " . $args['name']);
+});
+
+$app->get('/auth/{user}', function (Request $request, Response $response, $args) {
+
+
+	return $response->getBody()->write("Hello, " . $args['name']);
+});
+
+$app->post('/auth/user', function(Request $request, Response $response){
+	$name = $request->getParsedBodyParam('name');
+
+	if (empty($name)){
+		$response->withStatus(400)->withJson(['error' => "You must submit a 'name' field to create a user"]);
+	}
+
+	$token = random_bytes(6);
+	$token = bin2hex($token);
+
+	$this->sleek->users->insert(['name' => $name, 'token' => $token]);
+
+	return $response->withJson(['token' => $token]);
 });
 
 $app->run();
