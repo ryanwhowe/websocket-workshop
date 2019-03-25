a9a02f62c8f3058d5de5f43e13f2e82526273bb0
m1ke <git@m1ke.me>
2019-03-25 21:47:29 +0000

    Working app with database

--- lesson-4/php/index.php
+++ lesson-4/php/index.php
@@ -12,41 +12,75 @@ $config = [
 		'displayErrorDetails' => true,
 		'logger' => [
 			'name' => 'slim-app',
-			'level' => 'debug',
-			'path' => __DIR__ . '/logs/app.log',
+			'level' => 200,
+			'path' => __DIR__.'/logs/app.log',
 		],
 	],
 ];
 $app = new App($config);
 
 $container = $app->getContainer();
-$container['sleek'] = function($container){
-	$users = SleekDB::store('users', __DIR__.'/db');
+$container['sleek'] = function ($container){
+	$users = SleekDB::store('users', __DIR__.'/../db');
 
-	return (object) ['users' => $users];
+	return (object)['users' => $users];
 };
 
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-	return $response->getBody()->write("Hello, " . $args['name']);
+/* This route is just to test the web server works
+ *  Hit it from a browser or with `curl http://localhost:8082/hello/yorkshire`
+ */
+$app->get('/hello/{name}', function (Request $request, Response $response, $args){
+	return $response->getBody()->write("Hello, ".$args['name']);
 });
 
-$app->get('/auth/{user}', function (Request $request, Response $response, $args) {
+$app->get('/auth', function (Request $request, Response $response, $args){
+	$port = $request->getUri()->getPort();
+	// Port must be internal, prevents external snooping for tokens
+	if ($port!==80){
+		return $response->withStatus(403)->getBody()->write("Forbidden");
+	}
 
+	$name = $request->getQueryParam('name');
 
-	return $response->getBody()->write("Hello, " . $args['name']);
+	/**
+	 * @var SleekDB $users_db
+	 */
+	$users_db = $this->sleek->users;
+
+	$users = $users_db->where('name', '=', $name)->limit(1)->fetch();
+
+	if (empty($users)){
+		return $response->withStatus(404)->write('Not found');
+	}
+
+	return $response->getBody()->write($users[0]['token']);
 });
 
-$app->post('/auth/user', function(Request $request, Response $response){
+/* This route lets you add a user. The easiest way is using curl:
+ *   curl -X POST -d '{"name": "my user"}' -H 'content-type: application/json' localhost:8082/user
+ * You will receive a token in response
+ */
+$app->post('/user', function (Request $request, Response $response){
 	$name = $request->getParsedBodyParam('name');
 
 	if (empty($name)){
-		$response->withStatus(400)->withJson(['error' => "You must submit a 'name' field to create a user"]);
+		return $response->withStatus(400)->withJson(['error' => "You must submit a 'name' field to create a user"]);
+	}
+	/**
+	 * @var SleekDB $users_db
+	 */
+	$users_db = $this->sleek->users;
+
+	$users = $users_db->where('name', '=', $name)->fetch();
+
+	if ($users){
+		return $response->withStatus(400)->withJson(['error' => "The name '$name' is already taken"]);
 	}
 
 	$token = random_bytes(6);
 	$token = bin2hex($token);
 
-	$this->sleek->users->insert(['name' => $name, 'token' => $token]);
+	$users_db->insert(['name' => $name, 'token' => $token]);
 
 	return $response->withJson(['token' => $token]);
 });
