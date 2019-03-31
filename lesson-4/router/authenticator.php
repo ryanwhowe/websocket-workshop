a22961afe1145001a33bb6e6af0c245bff8a4b07
m1ke <git@m1ke.me>
2019-03-31 20:49:32 +0100

    Copy http utilities using guzzle

--- lesson-4/router/authenticator.php
+++ lesson-4/router/authenticator.php
@@ -1,5 +1,7 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Psr\Log\NullLogger;
 use Thruway\Authentication\ClientWampCraAuthenticator;
 use Thruway\ClientSession;
@@ -13,31 +15,20 @@ function terminal_log(string $msg){
 	echo "(Authenticator) $msg";
 }
 
-function http_get(string $url, array $headers = []){
-	$context = stream_context_create([
-		"http" => [
-			"method" => 'GET',
-			"header" => implode("\n", $headers),
-			"ignore_errors" => true,
-		],
-	]);
+function http_get(string $url){
+	$client = new Client();
 
-	$response = file_get_contents($url, false, $context);
-
-	/**
-	 * @var array $http_response_header
-	 */
-	$status_line = $http_response_header[0];
-
-	preg_match('{HTTP\/\S*\s(\d{3})}', $status_line, $match);
-
-	$status = $match[1];
-
-	if ($status!=="200"){
-		throw new Exception("Unexpected response status: {$status_line}\n".$response);
+	try {
+		$response = $client->get($url);
+	}
+	catch (RequestException $e) {
+		$response = $e->getResponse();
+		throw new Exception("Unexpected response status: {$response->getStatusCode()}\n\t{$response->getBody()}");
 	}
 
-	return $response;
+	$text = (string) $response->getBody();
+
+	return trim($text);
 }
 
 Logger::set(new NullLogger());
